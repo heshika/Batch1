97c53025d682a93ed1c90708c189161227287134
heshika <heshikatrainings@gmail.com>
2022-09-30 11:38:07 +0530

    day 3 data types concept

---
+++ sep30th_datatypes_day3.ps1
@@ -0,0 +1,90 @@
+﻿types
+
+1.special types : $null
+
+2. value types -data type :
+
+boolean [bool]
+integer -[int]
+string-[string]
+double-[double]
+float-[float]
+character -[char]
+long -[long]
+decimal -[decimal]
+
+Reference types :
+
+arrays,hashtables,
+
+xml data types
+
+
+xml
+
+regix and script blcok
+
+
+1.$hello
+#creating file
+2.new-item -path .\test.ps1 -Itemtype file
+#enter data or edit file 
+notepad.exe .\test.ps1
+#to see the data
+get-content -path 'c:\users\Indrani m\test.ps1'
+
+boolean 
+
+true or false
+
+$true=1
+$false=0
+
+if(Test-Path .\test10.ps1)
+{
+#create directory
+
+}
+
+
+
+
+
+
+1.badam
+
+price : 345rs 
+
+soap
+
+price : 200 rs 
+
+
+
+Typecasting :
+
+$service="something"
+
+[string]$service="something"
+
+
+$price="99.98"
+$price2=100
+
+[decimal]$price+[int]$price2
+
+
+$a="hello"
+$a="100"
+$a=200
+
+
+static data
+
+[string]$a="hello"
+[int]$a= 100
+
+
+array=collection of items,items can be different data types
+
+$array="sreenivas","maruthi","lab",100
---
+++ sep30thtest.ps1
@@ -0,0 +1,14 @@
+﻿#single quotes and double quotes
+
+#$productname="alexa"
+#write-host 'my product name $productname'
+
+$productname="alexa"
+write-host "my product name is $productname"
+
+<#
+note: to dispaly output of a variable always double quotes
+
+"double quotes" : interpreted
+'single quotes':  'not interpreted'
+#>
