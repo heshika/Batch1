81441dc6a01300464e99a9bc798601e1536398e0
heshika <heshikatrainings@gmail.com>
2022-10-04 11:36:07 +0530

    day 3 powershell

---
+++ day3-powershell/datatypes.ps1
@@ -0,0 +1,39 @@
+﻿datatypes:
+
+dynamic datatype :
+
+$a=100
+$a.GetType()
+
+$a="movie"
+
+$sugar_price="100"
+
+<#
+static data type : we define datatype explicitly
+
+[int]$a="text"
+
+[int]$sugar_price="100"
+
+example : you want calculate CPU utilizations
+
+
+$first_cpu_util =99.9
+
+$second="98"
+
+$c=$first_cpu_util+$second
+
+#>
+
+
+1.charcter
+2.strings
+3.double
+float
+integer
+strings
+arrays
+hastable
+regex
---
+++ day3-powershell/datatypes_1.ps1
@@ -0,0 +1,33 @@
+﻿[char]$a="a"
+[string]$b="testing"
+[int]$c=100
+[float]$d=99.8
+[double]$e=99.89
+[decimal]$f=99.9999
+
+#array =collection of elements is called array
+$arr=@()
+$narray ="one","two",123,300
+           0     1    2   3
+
+           -4       -3     -2   -1
+
+$narray[0]
+$narray[1]
+$narray[2]
+$narray[-4]
+
+hashtable : key value pair is called hastables
+name=test
+cpu=98
+ram=4gb
+
+$h=@{"name"="sony";"marks"="300";"percentage"="99"}
+$h.Name = "thanu"
+
+
+
+
+
+
+
