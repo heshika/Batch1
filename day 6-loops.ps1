703a0e69b5096da7adc144e451399b4cbd91340e
heshika <heshikatrainings@gmail.com>
2022-10-11 12:16:20 +0530

    add day 7 calss,flow control statements

--- day 6-loops.ps1
+++ day 6-loops.ps1
@@ -18,20 +18,20 @@ if($a -gt 5)
 
 example 2:
 
-$servicename =Get-Service|Where-Object{$_.Name -eq "chrome"}
+$servicename =Get-Service|Where-Object{$_.Name -eq "wsearch"}
 
-if($servicename.Name -eq "chrome")
+if($servicename.Name -contains "wsearch")
 {
-write-host "service   existis on my machine"
+write-host "service $($servicename.name)  existis on my machine"
 }
 
 example 3:
 
-if($false)
-{
- write-host "condition is true"
-}
+if($false){write-host "condition is true"}
+else{write-host "Condition is false"}
 
+if($true){write-host "condition is true"}
+else{write-host "condition is false"}
 
 Else :
 
@@ -80,6 +80,9 @@ assigmnet : how to find client entered number is even or odd
 
 5/2  = 1 (odd)
 
+$time =get-date
+If($time){write-host “today time is $time”}
+Else{write-host “the command for getting time failed or not executed”}
 
 
 
@@ -96,26 +99,25 @@ write-host "number $num is odd"
 #else-if
 #$a =50
 
-900
 
-$a =$null
-$a=Read-host -Prompt "enter some value"
-
-
+$a=99
 if($a -le 60){ write-host "$a is less  than or equals  60"}
-
-elseif($a -ge 60){write-host "$a is less than 60"}
-
-elseif($a -eq 60){write-host "$a is equal to 900"}
-
-elseif($a -lt 60){write-host "$a is graeter than or equakls to 600"}
-
-else{
-write-host "nothing matched"
-}
+elseif($a -gt 60){write-host "$a is greater 60"}
+elseif($a -eq 60){write-host "$a is equal to 60"}
+elseif($a -lt 60){write-host "$a is graeter than or equals to 60"}
+else{write-host "nothing matched"}
 
 #>
 
+
+$a=20;
+if($a -gt 10){write-host "$a is greater than 10" -ForegroundColor green}
+elseif($a -gt 20){write-host "$a greater than 20" -ForegroundColor green}
+elseif($a -gt 30){write-host "$a greater than 30 " -ForegroundColor green}
+elseif($a -gt 40){write-host "$a greater than 40" -ForegroundColor green}
+else{write-host "nothing matched"}
+
+i
 
 
 for :
---
+++ day-7 -forloop.ps1
@@ -0,0 +1,174 @@
+﻿for loop :
+
+repeation 
+
+for(condition){}
+
+for(intialization;termination condition;<count)
+{
+  copy file from one location to another location
+
+  c:users\indrani m \test to c:\users\desktop
+}
+
+
+
+
+
+0;0
+
+for($i=10;$i -ge 5;$i++)
+{
+ write-host "the value of i is $i"
+ Start-Sleep -Seconds 3
+}
+
+
+
+
+i=0;0 -le 5;1
+i=1;1 -le 5;2
+i=2;2 -le 5;3
+i=3;3 -le 5;4
+i=4;4 -le 5;5
+i=5;5 -le 5;6
+i=6;6 -le 5
+
+
+$a="*"
+$b="*"
+$a + $b
+
+
+$a ="devops"
+$b ="training"
+
+$a+$b
+
+$a ="*"
+$a++
+
+$a + "*"
+
+$a ="1"
+
+for($i=0;$i -le 10;$i++){
+ 
+#Write-Host "devops training" -ForegroundColor yellow
+#write-host "print value of i is $i" -ForegroundColor Green
+#Start-Sleep -Seconds  2
+##copying file
+#write-host " $i attempt of copying file "
+#Copy-Item -Path 'C:\Users\Indrani M\test' -Destination 'C:\Users\Indrani m\Documents'
+write-host "$a"
+$a= $a + "1"
+
+}
+
+
+
+how to print pattern 
+
+*
+**
+***
+*****
+******
+*******
+
+
+@
+@@
+@@@
+@@@@
+@@@@@
+
+
+$a ="@"
+
+for($i=0;$i -le 10;$i++){
+write-host "$a"
+$a= $a + "@"
+
+}
+
+
+how to find given number is even or odd number ?
+
+
+foreach: array of elements 
+
+$students ="thanuja","parimal","maruthi"
+foreach($student in $students)
+{
+write-host "my student $student got placed in devops job"
+start-sleep -Seconds 3
+}
+
+$servicenames ="VSS","wcmsvc","wsearch","wlansvc"
+foreach($service in $servicenames)
+{
+$servicestatus=Get-Service -Name $service |Select-Object  status
+write-host " $service status is $servicestatus"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
